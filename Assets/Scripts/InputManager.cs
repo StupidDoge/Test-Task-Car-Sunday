0e43cae76ac129bafcfdf6835b817135a1856ff4
Denis Litvinets <denis.litvinets@gmail.com>
2023-06-20 20:40:26 +0300

    rebuild input system

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,18 +1,74 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
-    private const string VERTICAL = "Vertical";
-    private const string HORIZONTAL = "Horizontal";
-
     public float Horizontal { get; private set; }
     public float Vertical { get; private set; }
     public bool IsBreaking { get; private set; }
 
-    private void Update()
+    public void ForwardInput(InputAction.CallbackContext context)
     {
-        Vertical = Input.GetAxis(VERTICAL);
-        Horizontal = Input.GetAxis(HORIZONTAL);
-        IsBreaking = Input.GetKey(KeyCode.Space);
+        if (context.started)
+        {
+            Vertical = 1;
+        }
+
+        if (context.canceled)
+        {
+            Vertical = 0;
+        }
+    }
+
+    public void BackwardInput(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            Vertical = -1;
+        }
+
+        if (context.canceled)
+        {
+            Vertical = 0;
+        }
+    }
+
+    public void RightInput(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            Horizontal = 1;
+        }
+
+        if (context.canceled)
+        {
+            Horizontal = 0;
+        }
+    }
+
+    public void LeftInput(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            Horizontal = -1;
+        }
+
+        if (context.canceled)
+        {
+            Horizontal = 0;
+        }
+    }
+
+    public void BreakInput(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            IsBreaking = true;
+        }
+
+        if (context.canceled)
+        {
+            IsBreaking = false;
+        }
     }
 }
